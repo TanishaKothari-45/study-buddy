6275e53d755ec715e9416e49c582790694261b2b
Tanisha <tanisha.k@zolostays.com>
2025-12-03 18:36:21 +0530

    err handling

--- backend/app/middleware/error_handler.py
+++ backend/app/middleware/error_handler.py
@@ -11,7 +11,7 @@ from fastapi.responses import JSONResponse
 from fastapi.exceptions import RequestValidationError
 from starlette.exceptions import HTTPException as StarletteHTTPException
 
-from .exceptions import AppException
+from ..core.exceptions import AppException
 
 # Configure logging
 logging.basicConfig(
