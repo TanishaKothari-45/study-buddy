1bc931bc39b8e3f6f697821ec7cca68ae32a91ae
sidDev798 <siddartha.r@turing.com>
2026-01-17 12:07:40 +0530

    added chache to docker

--- backend/app/main.py
+++ backend/app/main.py
@@ -102,6 +102,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
+        
     ],
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
